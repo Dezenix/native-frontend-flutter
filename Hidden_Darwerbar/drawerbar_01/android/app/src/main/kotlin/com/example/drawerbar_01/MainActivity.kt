0400356ccc3fb1ac8c3b2487c07f1f5862d119c2
navee <naveenarameshvardhini@gmail.com>
2021-11-28 17:25:40 +0530

    fix/Hidden Drawer bar

---
+++ Hidden_Darwerbar/drawerbar_01/android/app/src/main/kotlin/com/example/drawerbar_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drawerbar_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Hidden_Darwerbar/drawerbar_01/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DrawerPage()
+    );
+  }
+}
+class DrawerPage extends StatefulWidget {
+  const DrawerPage({ Key? key }) : super(key: key);
+
+  @override
+  _DrawerPageState createState() => _DrawerPageState();
+}
+
+class _DrawerPageState extends State<DrawerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
