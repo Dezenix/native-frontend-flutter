6615ab72b11c8c3649a59540f38734e4361867a8
navee <naveenarameshvardhini@gmail.com>
2021-12-01 11:04:15 +0530

    fix/Login

--- Login_Screens/Login01/lib/main.dart
+++ Login_Screens/Login01/lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const LoginPage(),
@@ -32,209 +21,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-
-
 class LoginPage extends StatefulWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  int activeIndex = 0;
-
-  @override
-  void initState() {
-    Timer.periodic(Duration(seconds: 5), (timer) {
-      setState(() {
-        activeIndex++;
-
-        if (activeIndex == 4)
-          activeIndex = 0;
-      });
-    });
-    
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              SizedBox(height: 50,),
-              Container(
-                height: 350,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: activeIndex == 0 ? 1 : 0, 
-                        duration: Duration(seconds: 1,),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/As6ct-Fovab32SIyMatjsqIaIjM9Jg1PblII8YAtBtQ/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTg4/LzNmMDU5Mzc0LTky/OTQtNDk5MC1hZGY2/LTA2YTkyMDZhNWZl/NC5zdmc.png', height: 400,),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: activeIndex == 1 ? 1 : 0, 
-                        duration: Duration(seconds: 1),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/vSx9H3yP2D4DgVoaFPbE4HVf6M4Phd-8uRjBZBnl83g/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNC84/MzcwMTY5OS1kYmU1/LTQ1ZmEtYmQ1Ny04/NTFmNTNjMTlkNTcu/c3Zn.png', height: 400,),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: activeIndex == 2 ? 1 : 0, 
-                        duration: Duration(seconds: 1),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/fv7W4YUUpGVcNhmKcDGZp6pF1-IDEyCjSjtBB8-Kp_0/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMTUv/ZjUzYTU4NDAtNjBl/Yy00ZWRhLWE1YWIt/ZGM1MWJmYjBiYjI2/LnN2Zw.png', height: 400,),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: activeIndex == 3 ? 1 : 0, 
-                        duration: Duration(seconds: 1),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/AVdOMf5ui4B7JJrNzYULVwT1z8NlGmlRYZTtg1F6z9E/rs:fit:784:767/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTY5/L2NlMTY1MWM5LTRl/ZjUtNGRmZi05MjQ3/LWYzNGQ1MzhiOTQ0/Mi5zdmc.png', height: 400,),
-                      ),
-                    )
-                  ]
-                ),
-              ),
-              SizedBox(height: 40,),
-              TextField(
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0.0),
-                  labelText: 'Email',
-                  hintText: 'Username or e-mail',
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14.0,
-                  ),
-                  prefixIcon: Icon(Iconsax.user, color: Colors.black, size: 18, ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  floatingLabelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.5),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextField(
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0.0),
-                  labelText: 'Password',
-                  hintText: 'Password',
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14.0,
-                  ),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  prefixIcon: Icon(Iconsax.key, color: Colors.black, size: 18, ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  floatingLabelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18.0,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.5),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {}, 
-                    child: Text('Forgot Password?', style: TextStyle(color: Colors.black, fontSize: 14.0, fontWeight: FontWeight.w400),),
-                  )
-                ],
-              ),
-              SizedBox(height: 30,),
-              MaterialButton(
-                onPressed: (){},
-                height: 45,
-                color: Colors.black,
-                child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 16.0),),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Don\'t have an account?', style: TextStyle(color: Colors.grey.shade600, fontSize: 14.0, fontWeight: FontWeight.w400),),
-                  TextButton(
-                    onPressed: () {}, 
-                    child: Text('Register', style: TextStyle(color: Colors.blue, fontSize: 14.0, fontWeight: FontWeight.w400),),
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
-      )
-    );
+    return Container();
   }
 }
