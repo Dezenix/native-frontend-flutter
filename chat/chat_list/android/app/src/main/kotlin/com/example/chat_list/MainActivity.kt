68b3a165ca593802b63d471b2972c2fd9d403865
navee <naveenarameshvardhini@gmail.com>
2021-12-06 19:16:40 +0530

    fix/Chatlist

---
+++ chat/chat_list/android/app/src/main/kotlin/com/example/chat_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chat/chat_list/lib/chatlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatList extends StatefulWidget {
+  const ChatList({Key? key}) : super(key: key);
+
+  @override
+  _ChatListState createState() => _ChatListState();
+}
+
+class _ChatListState extends State<ChatList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
