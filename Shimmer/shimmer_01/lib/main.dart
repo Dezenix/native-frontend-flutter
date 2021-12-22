2bd5feef2d6605a659defc64826cf38b18c116ca
navee-ramesh <54928117+naveeramesh@users.noreply.github.com>
2021-12-22 13:26:31 +0530

    Update main.dart

--- Shimmer/shimmer_01/lib/main.dart
+++ Shimmer/shimmer_01/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GeeksForGeeks',
+      title: 'Shimmer
       routes: {
         'loading': (_) => LoadingListPage(),
       },
