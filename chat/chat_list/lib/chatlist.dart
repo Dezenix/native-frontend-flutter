53d44de737ef05a46441d0f4fcdacca0a5b8b9a2
Sumi4482 <sbanwakde4482@gmail.com>
2022-01-09 14:23:36 +0530

    Revert "Added UI" This reverts commit 07a3634533548ff1b738284714bbc2c9df8c14c8.

--- chat/chat_list/lib/chatlist.dart
+++ chat/chat_list/lib/chatlist.dart
@@ -1,5 +1,3 @@
-import 'package:chat_list/recent_list.dart';
-import 'package:chat_list/search_bar.dart';
 import 'package:flutter/material.dart';
 
 class ChatList extends StatefulWidget {
@@ -12,65 +10,6 @@ class ChatList extends StatefulWidget {
 class _ChatListState extends State<ChatList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        leading: const Icon(
-          Icons.arrow_back,
-          color: Colors.black,
-        ),
-        title: const Text(
-          "Direct",
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 16.0),
-            child: Icon(
-              Icons.add,
-              size: 26.0,
-              color: Colors.black,
-            ),
-          ),
-        ],
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SearchBar(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-            Recent(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        height: 50,
-        color: Colors.white12,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(
-              Icons.camera_alt,
-              size: 30,
-              color: Colors.blue,
-            ),
-            Text(
-              'Camera',
-              style: TextStyle(color: Colors.blue, fontSize: 20),
-            )
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
